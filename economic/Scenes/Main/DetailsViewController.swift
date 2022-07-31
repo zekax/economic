555bfb6f7e3518a7cadadf68168b3db3b92fbcdc
zekax <jguilherme.00@gmail.com>
2022-08-01 00:39:52 +0100

    new receipt and details controllers

---
+++ economic/Scenes/Main/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  economic
+//
+//  Created by José Rodrigues on 01/08/2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ economic/Scenes/Main/NewReceiptViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewReceiptViewController.swift
+//  economic
+//
+//  Created by José Rodrigues on 01/08/2022.
+//
+
+import UIKit
+
+class NewReceiptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
